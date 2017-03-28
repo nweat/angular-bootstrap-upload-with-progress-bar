a6cceb32fbbaf04f3f37d53233df2ae5be436d23
nikki <nikkiweat@gmail.com>
2017-03-29 02:16:30 +0800

    add server setup

---
+++ index.js
@@ -0,0 +1,8 @@
+var express = require('express'),
+app = express();
+
+app.use(express.static(__dirname));
+app.get('/', function(req, res) {
+    res.sendfile('index.html', {root: __dirname })
+});
+var server = app.listen(process.env.PORT || 80);
